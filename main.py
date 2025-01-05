56e71be676c47fc32934c08f6d52ad2b1b7df674
Kevin Hayes Anderson <>
2025-01-05 19:50:08 +0530

    simplified (10 - total%10)
    include all digits in the total(sum of digits), for valid card numbers, total % 10 = 0

--- main.py
+++ main.py
@@ -9,41 +9,37 @@ Later:
     - Consider functional formulation(stream of digits -> map -> reduce)
     - Lookup table 
 """
-class LuhnAlgorithm():
-    # The Luhn algorithm or Luhn formula, also known as the "modulus 10" or "mod 10" algorithm, 
-    # is a simple checksum formula used to validate a variety of identification numbers, 
+
+
+class LuhnAlgorithm:
+    # The Luhn algorithm or Luhn formula, also known as the "modulus 10" or "mod 10" algorithm,
+    # is a simple checksum formula used to validate a variety of identification numbers,
     # such as credit card numbers, IMEI numbers, National Provider Identifier numbers in the United States, Canadian Social
     def validate_check_digit(payload: str) -> bool:
         pass
-    
+
     def calculate_check_digit(payload: str) -> str:
         pass
 
     @staticmethod
     def is_valid(payload: str) -> bool:
         total = 0
-        for index, d in enumerate(reversed(payload[:-1])): #TODO: ideally avoid slicing # since python creates a new list
-            if index % 2 == 0:
-                multiplier = 2
-            else:
-                multiplier = 1
-            x = int(d) * multiplier
-            xx = x // 10 + x % 10 # sum of digits # by doubling a single digit number, we get a number between 1 and 18(inclusive)
-            #for odd digits (single digits) eg: 9 % 10 = 9 # we ge the same number
-            total += xx
-        check_digit = (10 - (total % 10))
-        return check_digit == int(payload[-1])
-        
+        for index, d in enumerate(reversed(payload)):
+            x = int(d) * (1 + index % 2)
+            print(x)
+            # 4, 7, 9, 9, 2, 7, 3 ,9, 8, 7, 1
+            # 4, 14, 9, 18, 2, 14, 3, 18, 8, 14, 1
+            total += x // 10 + x  # 4+1+4+9+1+8+2+1+4+3+1+8+8+1+4+1
+        return total % 10 == 0  # 60 % 10 = 0
+
 
 def main():
-    
-    assert LuhnAlgorithm.is_valid("17893729974") 
+
+    assert LuhnAlgorithm.is_valid("17893729974")
     assert not LuhnAlgorithm.is_valid("17893729975")
     print("LuhnAlgorithm tests passed")
-    print('ok')
-    
+    print("ok")
+
+
 if __name__ == "__main__":
     main()
-
-
-
